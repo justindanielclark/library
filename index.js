f3d7672a7be1a120db9d7d196db2ea2766bb56e2
JClark <justindanielclark@gmail.com>
2022-09-21 05:41:03 -0700

    Initial Buildout

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log(`hit`);
+const Book = {
+    init(title, author, pageCount, hasRead){
+        this.title = title;
+        this.author = author;
+        this.pageCount = pageCount;
+        this.hasRead = hasRead
+    }
+}
+
+const Books = [
+    (Object.create(Book)).init(`All Quiet on the Western Front`, `Erich Maria Remarque`, 304, true),
+    (Object.create(Book)).init(`War and Peace`, `Leo Tolstoy`, 1152, false),
+    (Object.create(Book)).init(`Eloquent Javascript`, `Marjin Haverbeke`, 472, false),
+]
